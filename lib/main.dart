66a4f62a92a6deeaa843112305f2c369e6e6e840
Marta-24 <mllurbajane@gmail.com>
2022-10-30 10:44:51 +0100

    added title and widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,40 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  /*Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
             body: Center(
                 child: Container(
-      margin: const EdgeInsets.only(left: 15, top: 15, right: 15, bottom: 300),
-      color: Colors.amber[100],
-    ))));
+                    margin: const EdgeInsets.only(
+                        left: 15, top: 15, right: 15, bottom: 300),
+                    color: Colors.amber[100],
+                    child: const Center(
+                        child: Text(
+                      'The Goldfish',
+                      style: TextStyle(fontSize: 36.0, color: Colors.black),
+                    ))))));
+  }*/
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Container(
+            child: Center(
+      child: Column(children: <Widget>[
+        Container(
+            margin:
+                const EdgeInsets.only(left: 15, top: 15, right: 15, bottom: 30),
+            color: Colors.amber[100],
+            height: 600,
+            child: const Center(
+                child: Text(
+              'The Goldfish',
+              style: TextStyle(fontSize: 36.0, color: Colors.black),
+            ))),
+        const Text(
+          'Description',
+          style: TextStyle(fontSize: 24.0, color: Colors.black),
+        )
+      ]),
+    )));
   }
 }

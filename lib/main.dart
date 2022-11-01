00303ed47254aad210a4dd053297307ae5e9f0c7
Marta-24 <mllurbajane@gmail.com>
2022-11-01 17:35:17 +0100

    major changes

--- lib/main.dart
+++ lib/main.dart
@@ -10,40 +10,41 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  /*Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            body: Center(
-                child: Container(
-                    margin: const EdgeInsets.only(
-                        left: 15, top: 15, right: 15, bottom: 300),
-                    color: Colors.amber[100],
-                    child: const Center(
-                        child: Text(
-                      'The Goldfish',
-                      style: TextStyle(fontSize: 36.0, color: Colors.black),
-                    ))))));
-  }*/
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Container(
-            child: Center(
+        home: Center(
       child: Column(children: <Widget>[
         Container(
             margin:
-                const EdgeInsets.only(left: 15, top: 15, right: 15, bottom: 30),
-            color: Colors.amber[100],
+                const EdgeInsets.only(left: 15, top: 15, right: 15, bottom: 15),
             height: 600,
-            child: const Center(
-                child: Text(
-              'The Goldfish',
-              style: TextStyle(fontSize: 36.0, color: Colors.black),
-            ))),
-        const Text(
-          'Description',
-          style: TextStyle(fontSize: 24.0, color: Colors.black),
-        )
+            width: 10000,
+            decoration: BoxDecoration(
+              color: Colors.amber[100],
+            ),
+            child: Column(children: const <Widget>[
+              Text(
+                'IMAGE!!!',
+                style: TextStyle(fontSize: 16, color: Colors.red),
+              ),
+              Text('The Goldfish',
+                  style: TextStyle(fontSize: 36.0, color: Colors.black)),
+              Text('by Donna Tartt',
+                  style: TextStyle(fontSize: 16, color: Colors.grey)),
+              Text('PUNTUATION!!!',
+                  style: TextStyle(fontSize: 16, color: Colors.red)),
+            ])),
+        Column(children: const <Widget>[
+          Text(
+            'Description',
+            style: TextStyle(fontSize: 24.0, color: Colors.black),
+          ),
+          Text('Description here',
+              style: TextStyle(fontSize: 12, color: Colors.black)),
+          Text('do a row with the labels',
+              style: TextStyle(fontSize: 12, color: Colors.red)),
+        ])
       ]),
-    )));
+    ));
   }
 }
